552ab905093fb17d16e2170edf087a01f5ddb6ff
root <root@srv739706.hstgr.cloud>
2025-03-03 14:24:57 +0000

    Update code

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,10 @@ const nextConfig = {
   },
   // 增加静态页面生成超时时间
   staticPageGenerationTimeout: 120,
-  // 禁用一些静态页面生成
-  generateStaticParams: () => {
-    return []
-  },
+  // // 禁用一些静态页面生成
+  // generateStaticParams: () => {
+  //   return []
+  // },
   env: {
     USE_LOCAL_REDIS: process.env.USE_LOCAL_REDIS,
     LOCAL_REDIS_URL: process.env.LOCAL_REDIS_URL || 'redis://redis:6379'
